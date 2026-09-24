b4c3ecb365af50d52cd995647d40b47604b47af7
Oskar Cermeño <127172813+k0k4depelover@users.noreply.github.com>
2026-09-24 09:45:16 -0600

    Delete Ports/Out/RefrescarTokenUseCaseInterface.php

--- Ports/Out/RefrescarTokenUseCaseInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace App\Ports\In\Usuario\Refresh;
-
-interface RefrescarTokenUseCaseInterface
-{
-    public function execute(string $RefreshToken): RefreshTokenResponse;
-}
